75ea56a2b0a736a9bd11756e7ee750753c5ca8e1
madinam26 <144942320+madinam26@users.noreply.github.com>
2025-04-30 07:41:29 +0500

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,5 @@
+from sklearn.model_selection import train_test_split
+from sklearn_crfsuite import CRF, metrics
+
+# Заглушка тренировочного процесса
+print("Обучение модели на объединённых данных... (заглушка)")
